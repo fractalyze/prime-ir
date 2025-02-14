41c09c89f807c3c5c7c0c1e0529627fa420dabec
Batzorig Zorigoo <32958247+batzor@users.noreply.github.com>
2025-02-14 11:20:40 +0900

    feat: introduce mod-arith-to-arith lowering

---
+++ zkir/Dialect/ModArith/Conversions/ModArithToArith/ModArithToArith.h
@@ -0,0 +1,21 @@
+#ifndef ZKIR_DIALECT_MODARITH_TRANSFORMS_MODARITHTOARITH_H_
+#define ZKIR_DIALECT_MODARITH_TRANSFORMS_MODARITHTOARITH_H_
+
+#include "mlir/include/mlir/Dialect/Arith/IR/Arith.h"  // from @llvm-project
+#include "mlir/include/mlir/Pass/Pass.h"               // from @llvm-project
+
+namespace mlir {
+namespace zkir {
+namespace mod_arith {
+
+#define GEN_PASS_DECL
+#include "zkir/Dialect/ModArith/Conversions/ModArithToArith/ModArithToArith.h.inc"
+
+#define GEN_PASS_REGISTRATION
+#include "zkir/Dialect/ModArith/Conversions/ModArithToArith/ModArithToArith.h.inc"
+
+}  // namespace mod_arith
+}  // namespace zkir
+}  // namespace mlir
+
+#endif  // ZKIR_DIALECT_MODARITH_TRANSFORMS_MODARITHTOARITH_H_
