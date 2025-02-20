913ced72754f384ec145dfe7eb6ab17b375d463b
Batzorig Zorigoo <32958247+batzor@users.noreply.github.com>
2025-02-20 16:49:23 +0900

    feat(field): impl `prime-field-to-mod-arith` conversion

---
+++ zkir/Dialect/Field/Conversions/FieldToModArith/FieldToModArith.cpp
@@ -0,0 +1,146 @@
+#include "zkir/Dialect/Field/Conversions/FieldToModArith/FieldToModArith.h"
+
+#include <utility>
+
+#include "llvm/include/llvm/Support/Casting.h"  // from @llvm-project
+#include "mlir/include/mlir/Dialect/Affine/IR/AffineOps.h"  // from @llvm-project
+#include "mlir/include/mlir/Dialect/Linalg/IR/Linalg.h"  // from @llvm-project
+#include "mlir/include/mlir/Dialect/Tensor/IR/Tensor.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinAttributeInterfaces.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinAttributes.h"      // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinOps.h"             // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinTypeInterfaces.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinTypes.h"           // from @llvm-project
+#include "mlir/include/mlir/IR/ImplicitLocOpBuilder.h"   // from @llvm-project
+#include "mlir/include/mlir/IR/MLIRContext.h"            // from @llvm-project
+#include "mlir/include/mlir/IR/PatternMatch.h"           // from @llvm-project
+#include "mlir/include/mlir/IR/TypeUtilities.h"          // from @llvm-project
+#include "mlir/include/mlir/Support/LLVM.h"              // from @llvm-project
+#include "mlir/include/mlir/Support/LogicalResult.h"     // from @llvm-project
+#include "mlir/include/mlir/Transforms/DialectConversion.h"  // from @llvm-project
+#include "zkir/Dialect/Field/IR/FieldDialect.h"
+#include "zkir/Dialect/Field/IR/FieldOps.h"
+#include "zkir/Dialect/Field/IR/FieldTypes.h"
+#include "zkir/Dialect/ModArith/IR/ModArithDialect.h"
+#include "zkir/Dialect/ModArith/IR/ModArithOps.h"
+#include "zkir/Dialect/ModArith/IR/ModArithTypes.h"
+#include "zkir/Utils/ConversionUtils.h"
+
+namespace mlir {
+namespace zkir {
+namespace field {
+
+#define GEN_PASS_DEF_PRIMEFIELDTOMODARITH
+#include "zkir/Dialect/Field/Conversions/FieldToModArith/FieldToModArith.h.inc"
+
+mod_arith::ModArithType convertPrimeFieldType(PrimeFieldType type) {
+  IntegerAttr modulus = type.getModulus();
+  return mod_arith::ModArithType::get(type.getContext(), modulus);
+}
+
+Type convertPrimeFieldLikeType(ShapedType type) {
+  if (auto primeFieldType =
+          llvm::dyn_cast<PrimeFieldType>(type.getElementType())) {
+    return type.cloneWith(type.getShape(),
+                          convertPrimeFieldType(primeFieldType));
+  }
+  return type;
+}
+
+class PrimeFieldToModArithTypeConverter : public TypeConverter {
+ public:
+  PrimeFieldToModArithTypeConverter(MLIRContext *ctx) {
+    addConversion([](Type type) { return type; });
+    addConversion([](PrimeFieldType type) -> Type {
+      return convertPrimeFieldType(type);
+    });
+    addConversion([](ShapedType type) -> Type {
+      return convertPrimeFieldLikeType(type);
+    });
+  }
+};
+
+template <typename Op>
+TypedAttr modulusAttr(Op op) {
+  auto type = op.getResult().getType();
+  auto pfType = getResultPrimeFieldType(op);
+  APInt modulus = pfType.getModulus().getValue();
+
+  auto width = modulus.getBitWidth();
+
+  auto intType = IntegerType::get(op.getContext(), width);
+  auto truncmod = modulus.zextOrTrunc(width);
+
+  if (auto st = mlir::dyn_cast<ShapedType>(type)) {
+    auto containerType = st.cloneWith(st.getShape(), intType);
+    return DenseElementsAttr::get(containerType, truncmod);
+  }
+  return IntegerAttr::get(intType, truncmod);
+}
+
+template <typename Op>
+inline Type modulusType(Op op) {
+  return modulusAttr(op).getType();
+}
+
+struct ConvertConstant : public OpConversionPattern<ConstantOp> {
+  ConvertConstant(mlir::MLIRContext *context)
+      : OpConversionPattern<ConstantOp>(context) {}
+
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult matchAndRewrite(
+      ConstantOp op, OpAdaptor adaptor,
+      ConversionPatternRewriter &rewriter) const override {
+    ImplicitLocOpBuilder b(op.getLoc(), rewriter);
+
+    auto pfType = getResultPrimeFieldType(op);
+    auto modulus = pfType.getModulus();
+    auto modType = mod_arith::ModArithType::get(op.getContext(), modulus);
+    auto cval = b.create<mod_arith::ConstantOp>(
+        modType, op.getValue().getValue().getValue());
+    rewriter.replaceOp(op, cval);
+    return success();
+  }
+};
+
+namespace rewrites {
+// In an inner namespace to avoid conflicts with canonicalization patterns
+#include "zkir/Dialect/Field/Conversions/FieldToModArith/FieldToModArith.cpp.inc"
+}  // namespace rewrites
+
+struct PrimeFieldToModArith
+    : impl::PrimeFieldToModArithBase<PrimeFieldToModArith> {
+  using PrimeFieldToModArithBase::PrimeFieldToModArithBase;
+
+  void runOnOperation() override;
+};
+
+void PrimeFieldToModArith::runOnOperation() {
+  MLIRContext *context = &getContext();
+  ModuleOp module = getOperation();
+  PrimeFieldToModArithTypeConverter typeConverter(context);
+
+  ConversionTarget target(*context);
+  target.addIllegalDialect<FieldDialect>();
+  target.addLegalDialect<mod_arith::ModArithDialect>();
+
+  RewritePatternSet patterns(context);
+  rewrites::populateWithGenerated(patterns);
+  patterns.add<ConvertConstant>(typeConverter, context);
+
+  addStructuralConversionPatterns(typeConverter, patterns, target);
+
+  target.addDynamicallyLegalOp<
+      tensor::EmptyOp, tensor::ExtractOp, tensor::InsertOp, tensor::CastOp,
+      affine::AffineForOp, affine::AffineYieldOp, linalg::GenericOp,
+      linalg::YieldOp, tensor::ExtractSliceOp, tensor::InsertSliceOp>(
+      [&](auto op) { return typeConverter.isLegal(op); });
+
+  if (failed(applyPartialConversion(module, target, std::move(patterns)))) {
+    signalPassFailure();
+  }
+}
+}  // namespace field
+}  // namespace zkir
+}  // namespace mlir
---
+++ zkir/Dialect/Field/Conversions/FieldToModArith/FieldToModArith.h
@@ -0,0 +1,20 @@
+#ifndef ZKIR_DIALECT_FIELD_TRANSFORMS_FIELDTOMODARITH_H_
+#define ZKIR_DIALECT_FIELD_TRANSFORMS_FIELDTOMODARITH_H_
+
+#include "mlir/include/mlir/Pass/Pass.h"  // from @llvm-project
+
+namespace mlir {
+namespace zkir {
+namespace field {
+
+#define GEN_PASS_DECL
+#include "zkir/Dialect/Field/Conversions/FieldToModArith/FieldToModArith.h.inc"
+
+#define GEN_PASS_REGISTRATION
+#include "zkir/Dialect/Field/Conversions/FieldToModArith/FieldToModArith.h.inc"
+
+}  // namespace field
+}  // namespace zkir
+}  // namespace mlir
+
+#endif  // ZKIR_DIALECT_FIELD_TRANSFORMS_FIELDTOMODARITH_H_
