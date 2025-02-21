27e39bd0cd16d86d1882841a3bb1a07a7bdbc962
Batzorig Zorigoo <32958247+batzor@users.noreply.github.com>
2025-02-21 18:23:14 +0900

    feat(tools): add `ModArithDialect` support to lsp

--- tools/zkir-lsp.cpp
+++ tools/zkir-lsp.cpp
@@ -2,11 +2,12 @@
 #include "mlir/InitAllExtensions.h"
 #include "mlir/InitAllPasses.h"
 #include "mlir/Tools/mlir-lsp-server/MlirLspServerMain.h"
+#include "zkir/Dialect/ModArith/IR/ModArithDialect.h"
 
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
+  registry.insert<mlir::zkir::mod_arith::ModArithDialect>();
   mlir::registerAllDialects(registry);
   mlir::registerAllExtensions(registry);
-  // TODO(chokobole): add dialects
   return mlir::failed(mlir::MlirLspServerMain(argc, argv, registry));
 }
