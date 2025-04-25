1a21b2992af3c3f83d7d59870f4340fb90a6824b
ashjeong <ashjeong@umich.edu>
2025-04-25 14:27:39 +0900

    feat(elliptic_curve): convert EC types

--- zkir/Dialect/EllipticCurve/Conversions/EllipticCurveToField/EllipticCurveToField.cpp
+++ zkir/Dialect/EllipticCurve/Conversions/EllipticCurveToField/EllipticCurveToField.cpp
@@ -4,12 +4,78 @@
 
 #include "mlir/Transforms/DialectConversion.h"
 #include "zkir/Dialect/EllipticCurve/IR/EllipticCurveDialect.h"
+#include "zkir/Dialect/EllipticCurve/IR/EllipticCurveTypes.h"
 #include "zkir/Dialect/Field/IR/FieldDialect.h"
+#include "zkir/Dialect/Field/IR/FieldTypes.h"
 
 namespace mlir::zkir::elliptic_curve {
 
 #define GEN_PASS_DEF_ELLIPTICCURVETOFIELD
 #include "zkir/Dialect/EllipticCurve/Conversions/EllipticCurveToField/EllipticCurveToField.h.inc"
+
+//////////////// TYPE CONVERSION ////////////////
+
+RankedTensorType convertAffineType(AffineType type) {
+  field::PrimeFieldType baseField = type.getCurve().getA().getType();
+  return RankedTensorType::get({2}, baseField);
+}
+
+RankedTensorType convertJacobianType(JacobianType type) {
+  field::PrimeFieldType baseField = type.getCurve().getA().getType();
+  return RankedTensorType::get({3}, baseField);
+}
+
+RankedTensorType convertXYZZType(XYZZType type) {
+  field::PrimeFieldType baseField = type.getCurve().getA().getType();
+  return RankedTensorType::get({4}, baseField);
+}
+
+Type convertAffineLikeType(ShapedType type) {
+  if (auto affineType = dyn_cast<AffineType>(type.getElementType())) {
+    return type.cloneWith(type.getShape(), convertAffineType(affineType));
+  }
+  return type;
+}
+
+Type convertJacobianLikeType(ShapedType type) {
+  if (auto jacobianType = dyn_cast<JacobianType>(type.getElementType())) {
+    return type.cloneWith(type.getShape(), convertJacobianType(jacobianType));
+  }
+  return type;
+}
+
+Type convertXYZZLikeType(ShapedType type) {
+  if (auto xyzzType = dyn_cast<XYZZType>(type.getElementType())) {
+    return type.cloneWith(type.getShape(), convertXYZZType(xyzzType));
+  }
+  return type;
+}
+
+Type convertPointLikeType(ShapedType type) {
+  if (auto affineType = dyn_cast<AffineType>(type.getElementType())) {
+    return type.cloneWith(type.getShape(), convertAffineType(affineType));
+  } else if (auto jacobianType =
+                 dyn_cast<JacobianType>(type.getElementType())) {
+    return type.cloneWith(type.getShape(), convertJacobianType(jacobianType));
+  } else if (auto xyzzType = dyn_cast<XYZZType>(type.getElementType())) {
+    return type.cloneWith(type.getShape(), convertXYZZType(xyzzType));
+  }
+  return type;
+}
+
+class EllipticCurveToFieldTypeConverter : public TypeConverter {
+ public:
+  explicit EllipticCurveToFieldTypeConverter(MLIRContext *ctx) {
+    addConversion([](Type type) { return type; });
+    addConversion(
+        [](AffineType type) -> Type { return convertAffineType(type); });
+    addConversion(
+        [](JacobianType type) -> Type { return convertJacobianType(type); });
+    addConversion([](XYZZType type) -> Type { return convertXYZZType(type); });
+    addConversion(
+        [](ShapedType type) -> Type { return convertPointLikeType(type); });
+  }
+};
 
 namespace rewrites {
 // In an inner namespace to avoid conflicts with canonicalization patterns
@@ -25,6 +91,7 @@ struct EllipticCurveToField
 
 void EllipticCurveToField::runOnOperation() {
   MLIRContext *context = &getContext();
+  EllipticCurveToFieldTypeConverter typeConverter(context);
 
   ConversionTarget target(*context);
   target.addIllegalDialect<EllipticCurveDialect>();
