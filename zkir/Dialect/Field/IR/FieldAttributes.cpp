30fda4a5d33d5c62bca50e0d7907f9d8ebc180f9
Batzorig Zorigoo <32958247+batzor@users.noreply.github.com>
2025-05-12 16:46:52 +0900

    feat(field): add `RootOfUnityAttr`

--- zkir/Dialect/Field/IR/FieldAttributes.cpp
+++ zkir/Dialect/Field/IR/FieldAttributes.cpp
@@ -2,6 +2,7 @@
 
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinTypes.h"
+#include "zkir/Utils/APIntUtils.h"
 
 namespace mlir::zkir::field {
 
@@ -14,6 +15,22 @@ LogicalResult PrimeFieldAttr::verify(
         << "prime field modulus bitwidth does not match the value bitwidth";
     return failure();
   }
+  return success();
+}
+
+LogicalResult RootOfUnityAttr::verify(
+    ::llvm::function_ref<::mlir::InFlightDiagnostic()> emitError,
+    PrimeFieldAttr root, IntegerAttr degree) {
+  APInt modulus = root.getType().getModulus().getValue();
+  APInt rootOfUnity = root.getValue().getValue();
+  unsigned degreeValue = degree.getValue().getZExtValue();
+
+  if (!expMod(rootOfUnity, degreeValue, modulus).isOne()) {
+    emitError() << rootOfUnity.getZExtValue()
+                << " is not a root of unity of degree " << degreeValue;
+    return failure();
+  }
+
   return success();
 }
 
