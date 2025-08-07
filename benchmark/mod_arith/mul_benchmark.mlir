9e17176b49cad3900ebacd1514e2283e9685c46c
Batzorig Zorigoo <32958247+batzor@users.noreply.github.com>
2025-08-07 18:13:35 +0900

    refac(benchmark): use dot product for mul benchmark
    The previous benchmark was small and affected by benchmark overhead
    easily.

--- benchmark/mod_arith/mul_benchmark.mlir
+++ benchmark/mod_arith/mul_benchmark.mlir
@@ -1,18 +1,12 @@
 !mod = !mod_arith.int<21888242871839275222246405745257275088548364400416034343698204186575808495617 : i256>
 !modm = !mod_arith.int<21888242871839275222246405745257275088548364400416034343698204186575808495617 : i256, true>
 
-func.func @mul(%arg0 : i256) -> i256 attributes { llvm.emit_c_interface } {
-  %0 = mod_arith.encapsulate %arg0 : i256 -> !mod
-  %1 = mod_arith.mul %0, %0 : !mod
-  %2 = mod_arith.mul %0, %1 : !mod
-  %3 = mod_arith.extract %2 : !mod -> i256
-  return %3 : i256
+func.func @mul(%arg0: memref<1048576x!mod>, %arg1: memref<1048576x!mod>, %out: memref<!mod>) attributes { llvm.emit_c_interface } {
+  linalg.dot ins(%arg0, %arg1: memref<1048576x!mod>, memref<1048576x!mod>) outs(%out: memref<!mod>)
+  return
 }
 
-func.func @mont_mul(%arg0 : i256) -> i256 attributes { llvm.emit_c_interface } {
-  %0 = mod_arith.encapsulate %arg0 : i256 -> !modm
-  %1 = mod_arith.mont_mul %0, %0 : !modm
-  %2 = mod_arith.mont_mul %0, %1 : !modm
-  %3 = mod_arith.extract %2 : !modm -> i256
-  return %3 : i256
+func.func @mont_mul(%arg0 : memref<1048576x!modm>, %arg1 : memref<1048576x!modm>, %out: memref<!modm>) attributes { llvm.emit_c_interface } {
+  linalg.dot ins(%arg0, %arg1: memref<1048576x!modm>, memref<1048576x!modm>) outs(%out: memref<!modm>)
+  return
 }
--- benchmark/mod_arith/mul_benchmark_test.cc
+++ benchmark/mod_arith/mul_benchmark_test.cc
@@ -2,6 +2,8 @@
 #include "benchmark/benchmark.h"
 #include "gtest/gtest.h"
 
+#define N (1 << 20)
+
 namespace mlir::zkir::benchmark {
 namespace {
 
@@ -12,32 +14,46 @@ using i256 = BigInt<4>;
 const i256 kPrime = i256::fromHexString(
     "0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001");
 
-extern "C" void _mlir_ciface_mul(Memref<i256> *output, Memref<i256> *input);
-extern "C" void _mlir_ciface_mont_mul(Memref<i256> *output,
-                                      Memref<i256> *input);
+extern "C" void _mlir_ciface_mul(Memref<i256> *input1, Memref<i256> *input2,
+                                 Memref<i256> *output);
+extern "C" void _mlir_ciface_mont_mul(Memref<i256> *input1,
+                                      Memref<i256> *input2,
+                                      Memref<i256> *output);
+
+// Fill the input with random numbers in [0, `kPrime`).
+void fillWithRandom(Memref<i256> *input, const i256 &kPrime) {
+  // Set up the random number generator.
+  std::mt19937_64 rng(std::random_device{}());  // NOLINT(whitespace/braces)
+  std::uniform_int_distribution<uint64_t> dist(0, UINT64_MAX);
+  for (int i = 0; i < N; i++) {
+    *input->pget(i, 0) = i256::randomLT(kPrime, rng, dist);
+  }
+}
 
 template <bool kIsMont>
 void BM_mul_benchmark(::benchmark::State &state) {
-  Memref<i256> input(1, 1);
-
-  std::mt19937_64 rng(std::random_device{}());  // NOLINT(whitespace/braces)
-  std::uniform_int_distribution<uint64_t> dist(0, UINT64_MAX);
-
-  Memref<i256> output(1, 1);
+  Memref<i256> input1(N, 1);
+  Memref<i256> input2(N, 1);
+  Memref<i256> output(N, 1);
   for (auto _ : state) {
     state.PauseTiming();
-    *input.pget(0, 0) = i256::randomLT(kPrime, rng, dist);
+    fillWithRandom(&input1, kPrime);
+    fillWithRandom(&input2, kPrime);
     state.ResumeTiming();
     if constexpr (kIsMont) {
-      _mlir_ciface_mont_mul(&output, &input);
+      _mlir_ciface_mont_mul(&input1, &input2, &output);
     } else {
-      _mlir_ciface_mul(&output, &input);
+      _mlir_ciface_mul(&input1, &input2, &output);
     }
   }
 }
 
-BENCHMARK_TEMPLATE(BM_mul_benchmark, /*kIsMont=*/false)->Name("mul");
-BENCHMARK_TEMPLATE(BM_mul_benchmark, /*kIsMont=*/true)->Name("mont_mul");
+BENCHMARK_TEMPLATE(BM_mul_benchmark, /*kIsMont=*/false)
+    ->Unit(::benchmark::kMillisecond)
+    ->Name("mul");
+BENCHMARK_TEMPLATE(BM_mul_benchmark, /*kIsMont=*/true)
+    ->Unit(::benchmark::kMillisecond)
+    ->Name("mont_mul");
 
 }  // namespace
 }  // namespace mlir::zkir::benchmark
@@ -45,17 +61,17 @@ BENCHMARK_TEMPLATE(BM_mul_benchmark, /*kIsMont=*/true)->Name("mont_mul");
 // clang-format off
 // NOLINTBEGIN(whitespace/line_length)
 //
-// 2025-08-07T01:12:10+00:00
-// Run on AMD Ryzen 9 9950X3D (32 X 5529.37 MHz CPU s)
+// 2025-08-07T01:48:24+00:00
+// Run on AMD Ryzen 9 9950X3D (32 X 5479.99 MHz CPU s)
 // CPU Caches:
 //   L1 Data 48 KiB (x16)
 //   L1 Instruction 32 KiB (x16)
 //   L2 Unified 1024 KiB (x16)
 //   L3 Unified 98304 KiB (x2)
-// Load Average: 0.49, 8.35, 8.82
+// Load Average: 0.63, 2.23, 4.92
 // -----------------------------------------------------
 // Benchmark           Time             CPU   Iterations
 // -----------------------------------------------------
-// mul               124 ns          124 ns      5624619
-// mont_mul          128 ns          128 ns      5472464
+// mul              2031 ms         2031 ms            1
+// mont_mul         13.1 ms         13.1 ms           55
 // NOLINTEND()
