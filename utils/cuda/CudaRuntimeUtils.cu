7508285f287ef3ae53b192e58ff7247b88916e86
ashjeong <ashjeong@umich.edu>
2025-10-01 10:56:03 +0900

    feat(utils): port over `cub::DeviceRunLengthEncode::Encode`
    This will be used to help create offsets.

--- utils/cuda/CudaRuntimeUtils.cu
+++ utils/cuda/CudaRuntimeUtils.cu
@@ -1,5 +1,6 @@
 // clang-format off
 #include <cub/device/device_radix_sort.cuh>
+#include <cub/device/device_run_length_encode.cuh>
 // clang-format on
 
 #include <cstddef>
@@ -34,4 +35,32 @@ sortPairsI64I64(const int64_t *keysIn, int64_t *keysOut, const int64_t *valsIn,
                 int64_t *valsOut, int64_t numItems, CUstream inputStream) {
   sortPairs<int64_t, int64_t>(keysIn, keysOut, valsIn, valsOut, numItems,
                               inputStream);
+}
+
+template <typename Type>
+void Encode(const Type *in, Type *uniqueOut, Type *countsOut, Type *numRunsOut,
+            int64_t numRuns, CUstream inputStream) {
+  cudaStream_t stream = reinterpret_cast<cudaStream_t>(inputStream);
+  zkir::utils::CudaAsyncUniquePtr<std::byte> dTempStorage = nullptr;
+  size_t tempStorageBytes = 0;
+
+  // First call to DeviceRunLengthEncode::Encode determines the required size
+  // for temporary storage.
+  CHECK_CUDA_ERROR(cub::DeviceRunLengthEncode::Encode(
+      dTempStorage.get(), tempStorageBytes, in, uniqueOut, countsOut,
+      numRunsOut, numRuns, stream));
+
+  dTempStorage =
+      zkir::utils::makeCudaAsyncUnique<std::byte>(tempStorageBytes, stream);
+
+  // Second call to DeviceRunLengthEncode::Encode performs the actual encoding.
+  CHECK_CUDA_ERROR(cub::DeviceRunLengthEncode::Encode(
+      dTempStorage.get(), tempStorageBytes, in, uniqueOut, countsOut,
+      numRunsOut, numRuns, stream));
+}
+
+extern "C" MLIR_RUNNERUTILS_EXPORT void
+EncodeI64(const int64_t *in, int64_t *uniqueOut, int64_t *countsOut,
+          int64_t *numRunsOut, int64_t numRuns, CUstream inputStream) {
+  Encode<int64_t>(in, uniqueOut, countsOut, numRunsOut, numRuns, inputStream);
 }
