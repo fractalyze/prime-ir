21f8ceb73838826ca3a18f6edacd9c71e76dec5d
ashjeong <ashjeong@umich.edu>
2025-10-01 14:46:13 +0900

    test(benchmark): generate points that are actually on the curve
    Previously, cpu msm benchmark ran on randomly generated field values for the
    points which resulted in points not on the curve.

--- benchmark/msm/msm_benchmark.mlir
+++ benchmark/msm/msm_benchmark.mlir
@@ -1,3 +1,5 @@
+!PF = !field.pf<21888242871839275222246405745257275088696311157297823662689037894645226208583:i256>
+!SF = !field.pf<21888242871839275222246405745257275088548364400416034343698204186575808495617:i256>
 !PFm = !field.pf<21888242871839275222246405745257275088696311157297823662689037894645226208583:i256, true>
 !SFm = !field.pf<21888242871839275222246405745257275088548364400416034343698204186575808495617:i256, true>
 
@@ -15,6 +17,33 @@
 !SFm_M = memref<1048576x!SFm>
 !points_T = tensor<1048576x!affine>
 !points_M = memref<1048576x!affine>
+
+func.func private @getGeneratorMultiple(%k: !SF) -> !affine {
+  %onePF = field.constant 1 : !PF
+  %twoPF = field.constant 2 : !PF
+  %onePFm = field.to_mont %onePF : !PFm
+  %twoPFm = field.to_mont %twoPF : !PFm
+  %g = elliptic_curve.point %onePFm, %twoPFm : (!PFm, !PFm) -> !affine
+  %g_multiple = elliptic_curve.scalar_mul %k, %g : !SF, !affine -> !jacobian
+  %g_multiple_affine = elliptic_curve.convert_point_type %g_multiple : !jacobian -> !affine
+  return %g_multiple_affine : !affine
+}
+
+func.func private @generate_points(%rand_scalar : memref<!SF>, %points : !points_M) attributes { llvm.emit_c_interface } {
+  %c0 = arith.constant 0 : index
+  %c1 = arith.constant 1 : index
+  %rand_scalar_extracted = memref.load %rand_scalar[] : memref<!SF>
+  %first_point = func.call @getGeneratorMultiple(%rand_scalar_extracted) : (!SF) -> !affine
+
+  %num_scalar_muls = memref.dim %points, %c0 : !points_M
+  scf.for %i = %c0 to %num_scalar_muls step %c1 iter_args(%sum_iter = %first_point) -> (!affine){
+    %point = elliptic_curve.double %sum_iter : !affine -> !jacobian
+    %point_affine = elliptic_curve.convert_point_type %point : !jacobian -> !affine
+    memref.store %point_affine, %points[%i] : !points_M
+    scf.yield %point_affine : !affine
+  }
+  return
+}
 
 func.func @msm_serial(%scalars : !SFm_M, %points : !points_M) attributes { llvm.emit_c_interface } {
   %s = bufferization.to_tensor %scalars restrict writable : !SFm_M to !SFm_T
--- benchmark/msm/msm_benchmark_test.cc
+++ benchmark/msm/msm_benchmark_test.cc
@@ -13,11 +13,6 @@ namespace {
 
 using i256 = BigInt<4>;
 
-// `kPrime` =
-// 21888242871839275222246405745257275088548364400416034343698204186575808495617
-const i256 kPrimeBase = i256::fromHexString(
-    "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47");
-
 const i256 kPrimeScalar = i256::fromHexString(
     "0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001");
 
@@ -25,15 +20,13 @@ const i256 kPrimeScalar = i256::fromHexString(
 std::mt19937_64 rng(std::random_device{}()); // NOLINT(whitespace/braces)
 std::uniform_int_distribution<uint64_t> dist(0, UINT64_MAX);
 
-template <bool kIsScalar>
 void fillWithRandom(i256 &elem, ArrayRef<int64_t> coords) {
-  if constexpr (kIsScalar) {
-    elem = i256::randomLT(kPrimeScalar, rng, dist);
-  } else {
-    elem = i256::randomLT(kPrimeBase, rng, dist);
-  }
+  elem = i256::randomLT(kPrimeScalar, rng, dist);
 }
 
+extern "C" void
+_mlir_ciface_generate_points(StridedMemRefType<i256, 0> *rand_scalar,
+                             StridedMemRefType<i256, 2> *out);
 extern "C" void _mlir_ciface_msm_serial(StridedMemRefType<i256, 1> *scalars,
                                         StridedMemRefType<i256, 2> *points);
 extern "C" void _mlir_ciface_msm_parallel(StridedMemRefType<i256, 1> *scalars,
@@ -42,10 +35,13 @@ extern "C" void _mlir_ciface_msm_parallel(StridedMemRefType<i256, 1> *scalars,
 template <bool kIsParallel>
 void BM_msm_benchmark(::benchmark::State &state) {
   OwningMemRef<i256, 1> scalars(/*shape=*/{NUM_SCALARMULS}, /*shapeAlloc=*/{},
-                                /*init=*/fillWithRandom<true>);
+                                /*init=*/fillWithRandom);
+  OwningMemRef<i256, 0> randScalar(/*shape=*/{}, /*shapeAlloc=*/{},
+                                   /*init=*/fillWithRandom);
   OwningMemRef<i256, 2> points(/*shape=*/{NUM_SCALARMULS, 2},
-                               /*shapeAlloc=*/{},
-                               /*init=*/fillWithRandom<false>);
+                               /*shapeAlloc=*/{});
+
+  _mlir_ciface_generate_points(&*randScalar, &*points);
 
   for (auto _ : state) {
     if constexpr (kIsParallel) {
