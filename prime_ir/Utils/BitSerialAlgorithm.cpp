695a4ebd66add8211d85e451908ac5dab06d8102
Ryan Kim <chokobole33@gmail.com>
2026-01-28 00:06:04 +0900

    feat(utils): add BitSerialAlgorithm for bit-serial loop generation
    Add a shared utility for generating LSB-first binary method loops
    (double-and-add / square-and-multiply) with first-iteration unrolling.
    
    This utility can be used for:
    - Field exponentiation: doubleOp = square, accumulateOp = multiply
    - EC scalar multiplication: doubleOp = point double, accumulateOp = point add

---
+++ prime_ir/Utils/BitSerialAlgorithm.cpp
@@ -0,0 +1,103 @@
+/* Copyright 2026 The PrimeIR Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "prime_ir/Utils/BitSerialAlgorithm.h"
+
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+
+namespace mlir::prime_ir {
+
+Value generateBitSerialLoop(ImplicitLocOpBuilder &b, Value scalar, Value base,
+                            Value identity, DoubleCallback doubleOp,
+                            AccumulateCallback accumulateOp) {
+  Type scalarType = scalar.getType();
+  Type baseType = base.getType();
+
+  Value one = b.create<arith::ConstantIntOp>(scalarType, 1);
+  Value zero = b.create<arith::ConstantIntOp>(scalarType, 0);
+
+  // First iteration unrolling: handle LSB before entering the loop
+  Value result = identity;
+  Value lsb = b.create<arith::AndIOp>(scalar, one);
+  Value lsbSet = b.create<arith::CmpIOp>(arith::CmpIPredicate::ne, lsb, zero);
+
+  auto firstIfOp = b.create<scf::IfOp>(
+      lsbSet,
+      [&](OpBuilder &builder, Location loc) {
+        ImplicitLocOpBuilder ib(loc, builder);
+        Value newResult = accumulateOp(ib, result, base);
+        ib.create<scf::YieldOp>(newResult);
+      },
+      [&](OpBuilder &builder, Location loc) {
+        ImplicitLocOpBuilder ib(loc, builder);
+        ib.create<scf::YieldOp>(result);
+      });
+
+  result = firstIfOp.getResult(0);
+  scalar = b.create<arith::ShRUIOp>(scalar, one);
+
+  // Main loop: process remaining bits
+  auto whileOp = b.create<scf::WhileOp>(
+      /*resultTypes=*/TypeRange{scalarType, baseType, baseType},
+      /*operands=*/ValueRange{scalar, base, result},
+      /*beforeBuilder=*/
+      [&](OpBuilder &beforeBuilder, Location beforeLoc, ValueRange args) {
+        ImplicitLocOpBuilder ib(beforeLoc, beforeBuilder);
+        Value currentScalar = args[0];
+        // Continue while scalar > 0
+        Value cond = ib.create<arith::CmpIOp>(arith::CmpIPredicate::ugt,
+                                              currentScalar, zero);
+        ib.create<scf::ConditionOp>(cond, args);
+      },
+      /*afterBuilder=*/
+      [&](OpBuilder &afterBuilder, Location afterLoc, ValueRange args) {
+        ImplicitLocOpBuilder ib(afterLoc, afterBuilder);
+        Value currentScalar = args[0];
+        Value currentBase = args[1];
+        Value currentResult = args[2];
+
+        // Double the base
+        Value doubledBase = doubleOp(ib, currentBase);
+
+        // Check if current bit is set
+        Value bit = ib.create<arith::AndIOp>(currentScalar, one);
+        Value bitSet =
+            ib.create<arith::CmpIOp>(arith::CmpIPredicate::ne, bit, zero);
+
+        // Conditionally accumulate
+        auto ifOp = ib.create<scf::IfOp>(
+            bitSet,
+            [&](OpBuilder &builder, Location loc) {
+              ImplicitLocOpBuilder ib2(loc, builder);
+              Value newResult = accumulateOp(ib2, currentResult, doubledBase);
+              ib2.create<scf::YieldOp>(newResult);
+            },
+            [&](OpBuilder &builder, Location loc) {
+              ImplicitLocOpBuilder ib2(loc, builder);
+              ib2.create<scf::YieldOp>(currentResult);
+            });
+
+        // Shift scalar right
+        Value shiftedScalar = ib.create<arith::ShRUIOp>(currentScalar, one);
+
+        ib.create<scf::YieldOp>(
+            ValueRange{shiftedScalar, doubledBase, ifOp.getResult(0)});
+      });
+
+  return whileOp.getResult(2);
+}
+
+} // namespace mlir::prime_ir
