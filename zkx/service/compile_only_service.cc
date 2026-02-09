aa8e91b5e97f752325c428297c7f1cbeb72293e9
Ryan Kim <chokobole33@gmail.com>
2026-02-09 16:04:16 +0900

    feat(service): port CompileOnlyService from XLA
    Add CompileOnlyService, a Service specialization for ahead-of-time
    compilation that instantiates only a Compiler without requiring an
    execution backend. Supports CompileAheadOfTime with SPMD partitioning,
    static device assignment, and device shape representation.

---
+++ zkx/service/compile_only_service.cc
@@ -0,0 +1,143 @@
+/* Copyright 2017 The OpenXLA Authors.
+Copyright 2026 The ZKX Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "zkx/service/compile_only_service.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "absl/log/check.h"
+#include "absl/log/log.h"
+#include "absl/strings/str_cat.h"
+
+#include "zkx/debug_options_flags.h"
+#include "zkx/hlo/ir/hlo_module_group.h"
+#include "zkx/service/computation_layout.h"
+#include "zkx/service/dump.h"
+#include "zkx/service/platform_util.h"
+#include "zkx/status_macros.h"
+#include "zkx/util.h"
+
+namespace zkx {
+
+// static
+absl::StatusOr<std::unique_ptr<CompileOnlyService>>
+CompileOnlyService::NewService(se::Platform* platform) {
+  ServiceOptions default_options;
+  default_options.set_platform(platform);
+  return NewService(default_options);
+}
+
+// static
+absl::StatusOr<std::unique_ptr<CompileOnlyService>>
+CompileOnlyService::NewService(const ServiceOptions& options) {
+  se::Platform* platform = options.platform();
+  if (platform == nullptr) {
+    TF_ASSIGN_OR_RETURN(platform, PlatformUtil::GetDefaultPlatform());
+  }
+
+  TF_ASSIGN_OR_RETURN(Compiler * compiler, Compiler::GetForPlatform(platform));
+
+  std::unique_ptr<CompileOnlyService> service(
+      new CompileOnlyService(options, compiler));
+  return std::move(service);
+}
+
+CompileOnlyService::CompileOnlyService(const ServiceOptions& options,
+                                       Compiler* compiler)
+    : Service(options, /*execute_backend=*/nullptr), compiler_(compiler) {}
+
+absl::StatusOr<std::vector<std::unique_ptr<AotCompilationResult>>>
+CompileOnlyService::CompileAheadOfTime(
+    absl::Span<const AotZkxComputationInstance> computations,
+    const AotCompilationOptions& options,
+    std::unique_ptr<AotCompilationMetadata>* metadata) {
+  std::vector<std::unique_ptr<HloModule>> hlo_modules;
+
+  const DebugOptions& debug_options = options.debug_options();
+  ExecutionOptions execution_options;
+  *execution_options.mutable_debug_options() = debug_options;
+  // Capture replica_count, num_cores, and device_assignment in ExecutionOptions
+  // to later save in a proto dump.
+  if (options.replica_count() > 0) {
+    execution_options.set_num_replicas(options.replica_count());
+    if (options.has_static_device_assignment()) {
+      CHECK_EQ(options.replica_count(),
+               options.static_device_assignment().replica_count());
+    }
+  }
+  if (options.num_cores() > 0) {
+    execution_options.set_num_partitions(options.num_cores());
+    if (options.has_static_device_assignment()) {
+      CHECK_EQ(options.num_cores(),
+               options.static_device_assignment().computation_count());
+    }
+  }
+  if (options.has_static_device_assignment()) {
+    options.static_device_assignment().Serialize(
+        execution_options.mutable_device_assignment());
+  }
+  execution_options.set_use_spmd_partitioning(options.use_spmd_partitioning());
+  execution_options.set_use_auto_spmd_partitioning(
+      options.use_auto_spmd_partitioning());
+  for (auto t : options.auto_spmd_partitioning_mesh_shape()) {
+    execution_options.mutable_auto_spmd_partitioning_mesh_shape()->Add(t);
+  }
+  for (auto t : options.auto_spmd_partitioning_mesh_ids()) {
+    execution_options.mutable_auto_spmd_partitioning_mesh_ids()->Add(t);
+  }
+  execution_options.set_deduplicate_hlo(options.deduplicate_hlo());
+  for (const AotZkxComputationInstance& instance : computations) {
+    TF_RET_CHECK(instance.computation.has_host_program_shape());
+    auto update_shape_with_empty_tiles = [this](Shape* subshape,
+                                                const ShapeIndex& index) {
+      if (subshape->IsArray() &&
+          (!subshape->has_layout() || subshape->layout().tiles().empty())) {
+        *subshape = compiler_->DefaultDeviceShapeRepresentation(*subshape);
+      }
+    };
+    Shape result_layout(instance.result_layout);
+    ShapeUtil::ForEachMutableSubshape(&result_layout,
+                                      update_shape_with_empty_tiles);
+    *execution_options.mutable_shape_with_output_layout() =
+        result_layout.ToProto();
+    for (auto shape : instance.argument_layouts) {
+      ShapeUtil::ForEachMutableSubshape(const_cast<Shape*>(shape),
+                                        update_shape_with_empty_tiles);
+    }
+
+    TF_ASSIGN_OR_RETURN(
+        std::unique_ptr<HloModuleConfig> module_config,
+        CreateModuleConfig(
+            ProgramShape(instance.computation.host_program_shape()),
+            instance.argument_layouts, &execution_options, &options));
+
+    TF_ASSIGN_OR_RETURN(
+        std::unique_ptr<HloModule> hlo_module,
+        HloModule::CreateFromProto(instance.computation, *module_config));
+    DumpHloModuleIfEnabled(*hlo_module, "before_optimizations");
+    hlo_modules.push_back(std::move(hlo_module));
+  }
+
+  return compiler_->CompileAheadOfTime(
+      std::make_unique<HloModuleGroup>(hlo_modules[0]->name(),
+                                       absl::MakeSpan(hlo_modules)),
+      options, metadata);
+}
+
+}  // namespace zkx
---
+++ zkx/service/compile_only_service.h
@@ -0,0 +1,97 @@
+/* Copyright 2017 The OpenXLA Authors.
+Copyright 2026 The ZKX Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef ZKX_SERVICE_COMPILE_ONLY_SERVICE_H_
+#define ZKX_SERVICE_COMPILE_ONLY_SERVICE_H_
+
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/types/span.h"
+
+#include "zkx/service/backend.h"
+#include "zkx/service/compiler.h"
+#include "zkx/service/service.h"
+#include "zkx/stream_executor/stream_executor.h"
+#include "zkx/zkx_data.pb.h"
+
+namespace zkx {
+
+// A ZKX Service specialization for ahead-of-time compilation.  This only
+// instantiates a Compiler object for the relevant platform; it does not
+// instantiate or require an execution backend.
+class CompileOnlyService : public Service {
+ public:
+  // Factory for creating a CompileOnlyService. The parameter platform is the
+  // platform that the service should target. If platform is null then the
+  // default platform is used.
+  static absl::StatusOr<std::unique_ptr<CompileOnlyService>> NewService(
+      se::Platform* platform);
+  static absl::StatusOr<std::unique_ptr<CompileOnlyService>> NewService(
+      const ServiceOptions& options);
+
+  // A description of a zkx computation to compile using CompileAheadOfTime.
+  struct AotZkxComputationInstance {
+    HloModuleProto computation;
+    std::vector<const Shape*> argument_layouts;
+    Shape result_layout;
+  };
+
+  // Compiles a list of zkx computations for ahead-of-time execution.  This is
+  // intended for use in static compilation.  See
+  // |CompileOnlyClient::CompileAheadOfTime| for additional details.
+  absl::StatusOr<std::vector<std::unique_ptr<AotCompilationResult>>>
+  CompileAheadOfTime(absl::Span<const AotZkxComputationInstance> computations,
+                     const AotCompilationOptions& options,
+                     std::unique_ptr<AotCompilationMetadata>* metadata);
+
+  absl::StatusOr<std::vector<DeviceHandle>> GetDeviceHandles(
+      int64_t device_count) override {
+    return absl::UnimplementedError(
+        "CompileOnlyService does not support devices.");
+  }
+
+  absl::StatusOr<std::unique_ptr<GlobalData>> TransferToServer(
+      const LiteralSlice& literal_slice,
+      const DeviceHandle* device_handle) override {
+    return absl::UnimplementedError(
+        "CompileOnlyService does not support device data transfers.");
+  }
+
+  absl::Status TransferToInfeed(const LiteralSlice& literal, int64_t replica_id,
+                                const DeviceHandle* device_handle) override {
+    return absl::UnimplementedError(
+        "CompileOnlyService does not support device data transfers.");
+  }
+
+ private:
+  explicit CompileOnlyService(const ServiceOptions& options,
+                              Compiler* compiler);
+  CompileOnlyService(const CompileOnlyService&) = delete;
+  void operator=(const CompileOnlyService&) = delete;
+
+  // The compiler for the target platform.  This is included in place of
+  // the Service::execute_backend_'s compiler, since execute_backend_ is a
+  // nullptr in CompileOnlyService.
+  Compiler* compiler_;
+};
+
+}  // namespace zkx
+
+#endif  // ZKX_SERVICE_COMPILE_ONLY_SERVICE_H_
